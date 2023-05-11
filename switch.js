4f1143727ee30a7cc3f7992b6c1f3604dde7e81a
KruthikNBharadwaj <95702671+KruthikNBharadwaj@users.noreply.github.com>
2023-05-11 16:17:32 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,25 @@
+var day="Sunday";
+switch(day){
+    case 'Monday':
+        console.log("Busy day");
+        break;
+    case 'Tuesday':
+        console.log("Not so busy day");
+        break;
+    case 'Wednesday':
+        console.log("Mid week crisis");
+        break;
+    case 'Thursday':
+        console.log("Relax day");
+        break;
+    case 'Friday':
+        console.log("Party day");
+        break;
+    case 'Saturday':
+        console.log("Chill day");
+        break;
+    case 'Sunday':
+        console.log("No work day");
+        break;
+    default:console.log("There is no such day");
+}
