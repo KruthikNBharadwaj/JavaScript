51514e9e7e8ead5277f29feddbb1628ae27e9521
KruthikNBharadwaj <95702671+KruthikNBharadwaj@users.noreply.github.com>
2023-07-09 00:40:11 +0530

    Add files via upload

---
+++ Assignment2/assignment2.js
@@ -0,0 +1,44 @@
+// Task 1: Code a Person class
+class Person{
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+    }
+    doSomethingFun() {
+        this.energy -= 10;
+    }
+}
+// Task 2: Code a Worker class
+class Worker extends Person {
+  constructor(name = "Tom", age = 20, energy = 100, xp = 0, hourlyWage = 10) {
+    super(name, age, energy);
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+
+  goToWork() {
+    this.xp += 10;
+  }
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+  const internObject = new Worker("Bob", 21, 110, 0, 10);
+  internObject.goToWork();
+  return internObject;
+}
+
+// Task 4: Code a manager object, methods
+function manager() {
+  return new Worker("Alice", 30, 120, 100, 30);
+}
+const internObject = intern();
+console.log(internObject.name); // Output: Bob
+console.log(internObject.xp); // Output: 10
+
+const managerObject = manager();
+console.log(managerObject.name); // Output: Alice
+console.log(managerObject.xp); // Output: 100
